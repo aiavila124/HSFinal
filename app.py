fe7d4db74fa8c7f644f4030437679c33daabc71f
aiavila124 <aiavila124@gmail.com>
2023-05-23 16:11:07 -0500

    PRINT

--- app.py
+++ app.py
@@ -24,10 +24,13 @@ def admin(pagename):
 @app.route('/ruta-de-resultados', methods=['POST'])
 def procesar_archivo():
     archivo = request.files['mi_archivo'] 
+    print("Antess")
     if archivo.filename == '' or not archivo.filename.endswith('.xlsx'):
         return render_template('forms.html')   
     resultados_json = modelo.codigo_modelo(archivo)
+    print("Despuesss")
     os.remove(f"temp/{archivo.filename}")
+    print(f"temp/{archivo.filename}")
     return render_template('forms.html', resultados_json=resultados_json)
 
 @app.route('/descargar-archivo')
